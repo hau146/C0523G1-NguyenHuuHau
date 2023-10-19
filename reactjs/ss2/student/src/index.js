c95efd17c593b5177cb851034871177efcb6c764
hau146 <131940769+hau146@users.noreply.github.com>
2023-10-19 15:21:01 +0700

    'ok'

--- reactjs/ss2/student/src/index.js
+++ reactjs/ss2/student/src/index.js
@@ -5,53 +5,63 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const students = [
-  {
-    company: 'Alfreds Futterkiste',
-    contact: 'Maria Anders',
-    country: 'Germany'
-  },
-  {
-    company: 'Centro comercial Moctezuma',
-    contact: 'Francisco Chang',
-    country: 'Mexico'
-  },
-  {
-    company: 'Ernst Handel',
-    contact: 'Roland Mendel',
-    country: 'Austria'
-  },
-  {
-    company: 'Island Trading',
-    contact: 'Helen Bennett',
-    country: 'UK'
-  },
-  {
-    company: 'Laughing Bacchus Winecellars',
-    contact: 'Yoshi Tannamuri',
-    country: 'Canada'
-  },
-  {
-    company: 'Magazzini Alimentari Riuniti',
-    contact: 'Giovanni Rovelli',
-    country: 'Italy'
-  }
+    {
+        id: 1,
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        id: 2,
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        id: 3,
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        id: 4,
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        id: 5,
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        id: 6,
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
 ]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <table>
-      <tr>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
-      </tr>
-      {students.map(student => (
-          <tr>
-            <td>{student.company}</td>
-            <td>{student.contact}</td>
-            <td>{student.country}</td>
-          </tr>
-      ))}
+        <tr>
+            <thead>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            </thead>
+        </tr>
+        {students.map(student => (
+            <tr key={student.id}>
+                <tbody>
+                <td>{student.company}</td>
+                <td>{student.contact}</td>
+                <td>{student.country}</td>
+                </tbody>
+            </tr>
+        ))}
     </table>
 );
 
